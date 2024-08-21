693d9040445a406ff808b390731498b294e2f4bd
Joels-code <85315241+Joels-code@users.noreply.github.com>
2024-08-21 10:58:06 -0700

    * fixes android receiver

--- android-exoplayer/src/main/java/com/brentvatne/receiver/AudioBecomingNoisyReceiver.java
+++ android-exoplayer/src/main/java/com/brentvatne/receiver/AudioBecomingNoisyReceiver.java
@@ -25,7 +25,7 @@ public class AudioBecomingNoisyReceiver extends BroadcastReceiver {
     public void setListener(BecomingNoisyListener listener) {
         this.listener = listener;
         IntentFilter intentFilter = new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY);
-        context.registerReceiver(this, intentFilter);
+        compatRegisterReceiver(this.context, this, intentFilter, false);
     }
 
     public void removeListener() {
@@ -34,6 +34,14 @@ public class AudioBecomingNoisyReceiver extends BroadcastReceiver {
             context.unregisterReceiver(this);
         } catch (Exception ignore) {
             // ignore if already unregistered
+        }
+    }
+
+    private void compatRegisterReceiver(Context context, BroadcastReceiver receiver, IntentFilter filter, boolean exported) {
+        if (Build.VERSION.SDK_INT >= 34 && context.getApplicationInfo().targetSdkVersion >= 34) {
+            context.registerReceiver(receiver, filter, exported ? Context.RECEIVER_EXPORTED : Context.RECEIVER_NOT_EXPORTED);
+        } else {
+            context.registerReceiver(receiver, filter);
         }
     }
 }
